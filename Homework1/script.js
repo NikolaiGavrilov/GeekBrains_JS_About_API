dbbebf8237d7d4fc00a2468df1dce615fa5f4336
NikolaiGavrilov <nikolaisibir@mail.ru>
2024-08-23 16:50:54 +0300

    Completed Homework1

--- Homework1/script.js
+++ Homework1/script.js
@@ -32,19 +32,30 @@ fetch('data.json')
     .then(data => {
         data.forEach(activity => {
             classesEl.insertAdjacentHTML('afterbegin', `
-<div class="activity__content">
-<h3 class="activity__title">${activity.title}</h3>
-<p class="activity__time">Время мероприятия: ${activity.time}</p>
-<p class="activity__max-people">Максимальное кол-во участников: ${activity.max_people_num}</p>
-<p class="activity__current-people">Участников записано: ${activity.current_people_num}</p>
-<p class="activity__vacant-places">Свободно мест: ${activity.max_people_num - activity.current_people_num}</p>
-<button class="activity__buttonRegister">Записаться</button>
-<button class="activity__buttonCancel" style="display: none;">Отменить запись</button>
-</div>
+            <div class="card shadow-sm container activity__content" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title activity__title">${activity.title}</h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item activity__time">Время мероприятия: ${activity.time}</li>
+                    <li class="list-group-item activity__max-people">Максимальное кол-во участников: ${activity.max_people_num}</li>
+                    <li class="list-group-item activity__current-people">Участников записано: ${activity.current_people_num}</li>
+                    <li class="list-group-item activity__vacant-places">Свободно мест: ${activity.max_people_num - activity.current_people_num}</li>
+                </ul>
+                <div class="card-body d-flex  justify-content-center">
+                    <button class="activity__buttonRegister btn btn-outline-success">Записаться</button>
+                    <button class="activity__buttonCancel btn btn-outline-danger" style="display: none;">Отменить запись</button>
+                </div>
+            </div>
 `);
+
             let registerBtn = document.querySelector('.activity__buttonRegister')
             let cancelBtn = document.querySelector('.activity__buttonCancel');
 
+            if (activity.max_people_num - activity.current_people_num === 0) {
+                registerBtn.classList.replace('btn-outline-success', 'btn-outline-secondary');
+                registerBtn.setAttribute('disabled', '');
+            }
             registerBtn.addEventListener('click', function (e) {
                 const necessaryClass = registerBtn.closest('.activity__content');
                 if (activity.max_people_num - activity.current_people_num > 0 && activity.your_participation != true) {
@@ -53,7 +64,10 @@ fetch('data.json')
                     necessaryClass.querySelector('.activity__vacant-places').textContent = `Свободно мест: ${activity.max_people_num - activity.current_people_num}`;
                     necessaryClass.querySelector('.activity__current-people').textContent = `Участников записано: ${activity.current_people_num}`;
                     necessaryClass.querySelector('.activity__buttonCancel').style.display = 'block';
+                    registerBtn.classList.replace('btn-outline-success', 'btn-outline-secondary');
+                    registerBtn.setAttribute('disabled', '');
                 } else if (activity.your_participation === true) {
+                    e.preventDefault();
                     alert('Вы уже записались на это мероприятие');
                 }
                 else {
@@ -68,9 +82,10 @@ fetch('data.json')
                 necessaryClass.querySelector('.activity__vacant-places').textContent = `Свободно мест: ${activity.max_people_num - activity.current_people_num}`;
                 necessaryClass.querySelector('.activity__current-people').textContent = `Участников записано: ${activity.current_people_num}`;
                 cancelBtn.style.display = 'none';
+                registerBtn.classList.replace('btn-outline-secondary', 'btn-outline-success');
+                registerBtn.removeAttribute('disabled');
             });
 
         });
     })
     .catch(error => console.error('Ошибка загрузки данных:', error));
-
